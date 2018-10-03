2a73babdfee45fff71df3a24777afb3af66fce1c
吳登秝 <ji3g4kami@gmail.com>
2018-10-03 09:27:10 +0800

    Initial Commit

---
+++ PostTime/PostTime.h
@@ -0,0 +1,19 @@
+//
+//  PostTime.h
+//  PostTime
+//
+//  Created by udn on 2018/10/3.
+//  Copyright © 2018 dengli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PostTime.
+FOUNDATION_EXPORT double PostTimeVersionNumber;
+
+//! Project version string for PostTime.
+FOUNDATION_EXPORT const unsigned char PostTimeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PostTime/PublicHeader.h>
+
+
